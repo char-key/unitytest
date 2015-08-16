a80708c190a3d8479d0a1ac3aaea62feb5cc89af
abrahamvarricatt <abraham.varricatt@googlemail.com>
2015-08-16 17:22:15 +0530

    Skeleton for Unity
    2 scenes for number guessing game (only 1 works now..)

---
+++ KanDan/Assets/GameMasterBehavior.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameMasterBehavior : MonoBehaviour {
+
+	Text chosenText, lowerText, upperText;
+	Button guessButton;
+	Slider slider;
+
+	MakeNumber gameLogic;
+
+	// Use this for initialization
+	void Start () {
+
+		gameLogic = new MakeNumber ();
+
+		//Find the Text
+		GameObject textGameObject = GameObject.Find ("ChosenNumberText");
+		chosenText = textGameObject.GetComponent<Text> ();
+		
+		if (chosenText == null) {
+			Debug.LogError("GameMasterBehavior is not attached to an Output text");
+		}
+
+		//Find the lowerLimit Text
+		GameObject lowerTextGameObject = GameObject.Find ("LowerNumberText");
+		lowerText = lowerTextGameObject.GetComponent<Text> ();
+		
+		if (lowerText == null) {
+			Debug.LogError("GameMasterBehavior is not attached to an Output text");
+		}
+
+		//Find the upperLimit Text
+		GameObject upperTextGameObject = GameObject.Find ("UpperNumberText");
+		upperText = upperTextGameObject.GetComponent<Text> ();
+		
+		if (upperText == null) {
+			Debug.LogError("GameMasterBehavior is not attached to an Output text");
+		}
+
+		//Find the Button
+		GameObject buttonGameObject = GameObject.Find ("GuessButton");
+		guessButton = buttonGameObject.GetComponent<Button> ();
+		
+		if (guessButton == null) {
+			Debug.LogError("GameMasterBehavior is not attached to a button");
+		}
+
+		//Add the event handler
+		guessButton.onClick.AddListener (guessAction);
+
+		//Find the Slider
+		GameObject sliderGameObject = GameObject.Find ("GuessSlider");
+		slider = sliderGameObject.GetComponent<Slider> ();
+		
+		if (slider == null) {
+			Debug.LogError("GameMasterBehavior is not attached to a slider");
+		}
+
+		gameInitialize ();
+	}
+
+	void gameInitialize(){
+		int lowerBound, upperBound;
+		
+		lowerBound = gameLogic.Range_min;
+		upperBound = gameLogic.Range_max;
+		
+		Debug.Log("Lower: " + lowerBound + ", Upper: " + upperBound);
+
+		int firstChosen  = (lowerBound + upperBound) / 2; 
+		chosenText.text = firstChosen.ToString ();
+		
+		slider.minValue = lowerBound;
+		slider.maxValue = upperBound;
+		
+		lowerText.text = lowerBound.ToString();
+		upperText.text = upperBound.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void guessAction(){
+
+		//TODO: Secure this line of code
+		int numberChosen = int.Parse (chosenText.text);
+		Debug.Log ("Number Guessed: " + numberChosen);
+
+
+		int result = gameLogic.Guess (numberChosen);
+		Debug.Log ("result = " + result);
+
+		if (result == 0) {
+			//TODO: Win Screen
+			Debug.Log("You Win!");
+		} else {
+			//Continue the game
+			int lowerBound, upperBound;
+
+			lowerBound = gameLogic.Range_min;
+			upperBound = gameLogic.Range_max;
+
+			Debug.Log("Lower: " + lowerBound + ", Upper: " + upperBound);
+
+			slider.minValue = lowerBound;
+			slider.maxValue = upperBound;
+
+			lowerText.text = lowerBound.ToString();
+			upperText.text = upperBound.ToString();
+		}
+	}
+}
---
+++ KanDan/Assets/MakeNumber.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MakeNumber {
+
+	// number to guess - should be between Range_min and Range_max
+	// if negative, means it hasn't been randomized
+	private int number = -1;
+	
+	// number of attempts
+	private int _attempts = 0;
+	public int Attempts
+	{
+		get
+		{
+			return this._attempts;
+		}
+		private set
+		{
+			this._attempts = value;
+		}
+	}
+	
+	// minimum range value
+	private int _range_min = 0;
+	public int Range_min
+	{
+		get
+		{
+			return this._range_min;
+		}
+		set
+		{
+			if (value < 0)
+			{
+				throw new ArgumentOutOfRangeException("Cannot set minimum range smaller than zero!!");
+			}
+			else if (value >= this.Range_max)
+			{
+				throw new ArgumentOutOfRangeException("Cannot set minimum range >= maximum");
+			}
+			this._range_min = value;
+			makeNumber(); // reset the random number
+		}
+	}
+	
+	// maximum range value
+	private int _range_max = 10;
+	public int Range_max
+	{
+		get
+		{
+			return this._range_max;
+		}
+		set
+		{
+			if (value > 1000)
+			{
+				throw new ArgumentOutOfRangeException("Cannot set maximum range greater than 1000!!");
+			}
+			else if (value <= this.Range_min)
+			{
+				throw new ArgumentOutOfRangeException("Cannot set maximum range <= minimum");
+			}
+			this._range_max = value;
+			makeNumber(); // reset random number
+		}
+	}
+	
+	
+	private void makeNumber()
+	{
+		// http://stackoverflow.com/a/2706537		
+		number = UnityEngine.Random.Range (this.Range_min, this.Range_max);	
+		this.Attempts = 0; // reset number of attempts
+		Debug.Log(">>>>>>>>>>>>>>>> " + number);
+	}
+
+	public int Guess(int guess_num)
+	{
+		if (number < 0)
+		{
+			// make a new random number
+			makeNumber();
+		}
+		
+		// increment number of attempts
+		this.Attempts++;
+		
+		// How accurate is the guess?
+		int diff = guess_num - number;
+		
+		if (diff == 0)
+		{
+			// reset the number since we've guessed it
+			number = -1;
+			// read Attempts to see how many guesses it took
+			// BEFORE trying out a new guess
+		}
+		
+		return diff;
+	}
+}
